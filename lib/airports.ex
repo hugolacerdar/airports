cc7e1be475a1719e4fdf1a2aa4c402c638d301ef
Hugo <hugolacerdar@gmail.com>
2021-08-25 16:36:22 -0300

    Add trigger

--- lib/airports.ex
+++ lib/airports.ex
@@ -26,11 +26,22 @@ defmodule Airports do
       }
     end)
     |> Flow.reject(&(&1.type == "closed"))
-    |> Flow.partition(key: {:key, :country})
+    |> Flow.partition(window: window, key: {:key, :country})
     |> Flow.group_by(& &1.country)
-    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
-    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+        |> IO.inspect(label: inspect(self()))
+
+      case trigger do
+        :done ->
+          {events, acc}
+
+        {:every, 1000} ->
+          {[], acc}
+      end
+    end)
     |> Enum.to_list()
-    |> List.flatten()
   end
 end
